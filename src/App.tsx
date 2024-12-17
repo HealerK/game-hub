f60d987a8dc0694e4599f3a589e6502fb06d9b46
HealerK <shinemyatshwe@gmail.com>
2024-12-17 07:31:51 +0630

    Responsive Layout Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import { Button } from "@chakra-ui/react/button";
+import { useBreakpointValue } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react/grid";
+import Navbar from "./components/ui/Navbar";
 
 function App() {
-  return <Button colorPalette="blue">Hello</Button>;
+  const showAside = useBreakpointValue({ base: false, lg: true });
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        <Navbar />
+      </GridItem>
+      {showAside && (
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      )}
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
