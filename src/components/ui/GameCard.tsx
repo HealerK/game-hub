846c4be84ed751b78b945ab4b8fc1cabbd5174c2
HealerK <shinemyatshwe@gmail.com>
2025-01-03 16:38:02 +0630

    Refactor: Removed Duplicate Styles

--- src/components/ui/GameCard.tsx
+++ src/components/ui/GameCard.tsx
@@ -10,11 +10,11 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root width="300px" borderRadius={10} overflow="hidden">
+    <Card.Root>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between" marginBottom={3}>
+        <HStack justifyContent="space-between" marginY={2}>
           <PlatformIconList
             platforms={game.parent_platforms?.map((p) => p.platform)}
           />
