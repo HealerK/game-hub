c713742ab7e1a210c1e2fc2323e2d674ca86d746
HealerK <shinemyatshwe@gmail.com>
2025-01-09 18:24:58 +0630

    Final Fixing

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Flex, Image } from "@chakra-ui/react";
-import logo from "../../assets/logo.webp";
+import logo from "../../assets/logo.jpg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
@@ -10,7 +10,7 @@ interface Props {
 const Navbar = ({ onSearch }: Props) => {
   return (
     <Flex padding="10px" justifyContent="space-between">
-      <Image src={logo} height="60px" />
+      <Image src={logo} height="50px" borderRadius={10} marginTop={1} />
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </Flex>
